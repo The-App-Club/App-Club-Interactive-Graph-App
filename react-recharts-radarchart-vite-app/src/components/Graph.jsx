88d871dd9afff5b54434d73de4cb666d7b0e156a
Higashi-Kota <higashi.kota@gri.jp>
2022-07-10 15:10:20 +0900

    add sample

---
+++ react-recharts-radarchart-vite-app/src/components/Graph.jsx
@@ -0,0 +1,92 @@
+import * as d3 from 'd3';
+import {css} from '@emotion/css';
+import {useState} from 'react';
+import {
+  Radar,
+  RadarChart,
+  PolarGrid,
+  PolarAngleAxis,
+  PolarRadiusAxis,
+  ResponsiveContainer,
+} from 'recharts';
+
+const data = [
+  {
+    subject: 'Math',
+    A: 120,
+    B: 110,
+    fullMark: 150,
+  },
+  {
+    subject: 'Chinese',
+    A: 98,
+    B: 130,
+    fullMark: 150,
+  },
+  {
+    subject: 'English',
+    A: 86,
+    B: 130,
+    fullMark: 150,
+  },
+  {
+    subject: 'Geography',
+    A: 99,
+    B: 100,
+    fullMark: 150,
+  },
+  {
+    subject: 'Physics',
+    A: 85,
+    B: 90,
+    fullMark: 150,
+  },
+  {
+    subject: 'History',
+    A: 65,
+    B: 85,
+    fullMark: 150,
+  },
+];
+
+const COLORS = d3.schemeTableau10;
+const Graph = () => {
+  const [resized, setResized] = useState(new Date());
+
+  const handleResize = (e) => {
+    setResized(new Date());
+  };
+
+  useState(() => {
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  return (
+    <ResponsiveContainer
+      className={css`
+        width: 100%;
+        min-height: 300px;
+        margin: 30px 0 0 -10px;
+      `}
+    >
+      <RadarChart cx="50%" cy="50%" outerRadius="80%" data={data}>
+        <PolarGrid />
+        <PolarAngleAxis dataKey="subject" />
+        <PolarRadiusAxis />
+        <Radar
+          name="Mike"
+          dataKey="A"
+          stroke="#8884d8"
+          fill="#8884d8"
+          fillOpacity={0.6}
+        />
+      </RadarChart>
+    </ResponsiveContainer>
+  );
+};
+
+export {Graph};
